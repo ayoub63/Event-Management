61913cc1a3aada5e49a754c4d5ad91a447a9c291
ayoub63 <ayoubafkir18@gmail.com>
2024-08-21 16:14:12 +0200

    Notification Controller für Requests

---
+++ eventmanagement/src/main/java/com/example/eventmanagement/Controller/NotificationController.java
@@ -0,0 +1,24 @@
+package com.example.eventmanagement.Controller;
+
+import com.example.eventmanagement.service.NotificationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/notify")
+public class NotificationController {
+
+    @Autowired
+    private NotificationService notificationService;
+
+    @PostMapping
+    public ResponseEntity<String> sendNotification(@RequestParam String email, @RequestParam String eventName) {
+        try {
+            notificationService.sendEmail(email, eventName);
+            return ResponseEntity.ok("Nachricht erfolgreich gesendet");
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("Senden der Nachricht fehlgeschlagen");
+        }
+    }
+}
